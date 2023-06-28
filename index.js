849598110e7f858ec275172d718d8e1a8c3ab198
goku <wkurts247@gmail.com>
2023-06-28 11:40:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(arr, str) {
+    const newDrivers = [];
+    for (const item of arr) {
+        if (item.toLowerCase() === str.toLowerCase()) {
+            newDrivers.push(item)
+        }
+    }
+    return newDrivers;
+}
+
+console.log(findMatching(drivers, "Bobby"));
+
+function fuzzyMatch(arr, str) {
+    const newDrivers = [];
+    for(const item of arr) {
+        if (item[0] === str[0]) {
+            newDrivers.push(item)
+        }
+    }
+    return newDrivers;
+}
+
+console.log(fuzzyMatch(drivers, "Sally"));
+
+const drivers1 = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(obj, str) {
+    const newDrivers = [];
+    for (const item of obj) {
+        if (item.name === str) {
+            newDrivers.push(item);
+        }
+    }
+    return newDrivers;
+}
+
+console.log(matchName(drivers1, "Sally"));
